6263b3a8a3aa529cdf5e1c01b87b480a8d37a4d0
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2023-03-01 16:15:56 +0600

    Expression validation app - phone number validation added

--- expressionValidation/script.js
+++ expressionValidation/script.js
@@ -12,15 +12,32 @@ postalCodeButton.addEventListener('click',validatePostalCode);
 function validateEmail(e){
     let exp = prompt('Enter your email: ');
     let re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
-    if (re.test(exp)){
-        alert('Your email address is valid!');
+    if (exp===''){
+        alert('You didn\'t enter your email!');
+    } else if (exp===null){
+        alert('You have cancelled email validation!');
     } else {
-        alert('Your email address is invalid!');
+        if (re.test(exp)){
+            alert('Your email address is valid!');
+        } else {
+            alert('Your email address is invalid!');
+        }
     }
 }
 function validatePhoneNumber(e){
     let exp = prompt('Enter your phone number: ');
-    console.log(exp);
+    let re = /(^(\+8801|8801|01|008801))[1|3-9]{1}(\d){8}$/;
+    if (exp===''){
+        alert('You didn\'t enter your phone number!');
+    } else if (exp===null){
+        alert('You have cancelled phone number validation!');
+    } else {
+        if (re.test(exp)){
+            alert('Your phone number is a valid Bangladeshi phone number!');
+        } else {
+            alert('Your phone number is not a valid Bangladeshi phone number!');
+        }
+    }
 }
 function validatePostalCode(e){
     let exp = prompt('Enter your postal code: ');
